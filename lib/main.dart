7281575e34db81b005e54eda4bdaa28dac3c71a3
AhmedAskere <87516271+AhmedAskere@users.noreply.github.com>
2021-11-01 23:43:24 +0300

    Add boxes and text

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
 
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
                 backgroundImage: AssetImage('images/22.jpg'),
               ),
               Text(
-                'احمد العسكر',
+                'احمد جاسم العسكر الشمري',
                 style: TextStyle(
                   fontFamily: 'IBMPlexSansArabic',
                   fontSize: 30.0,
@@ -34,12 +34,93 @@ class MyApp extends StatelessWidget {
                 ),
               ),
               Text(
-                'احمد العسكر',
+                'مبرمج تطبيقات سطح المكتب (دسكتوب) و مواقع الويب (ويب سايت )و تطبيقات الموبايل ( اندرويد و ايفون)',
                 style: TextStyle(
                   fontFamily: 'IBMPlexSansArabic',
-                  fontSize: 30.0,
+                  fontSize: 20.10,
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 50, left: 20, right: 20),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Row(
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      size: 40.0,
+                      color: Colors.cyan[700],
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text(
+                      '(+964) 781 364 6675',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                        fontFamily: 'IBMPlexSansArabic',
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Row(
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    Icon(
+                      Icons.email,
+                      size: 40.0,
+                      color: Colors.cyan[700],
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text(
+                      'ahmed.al.shmary.6675@gmail.com',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 17.0,
+                        fontFamily: 'IBMPlexSansArabic',
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+                padding: EdgeInsets.all(5.0),
+                color: Colors.white,
+                child: Row(
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    Icon(
+                      Icons.message,
+                      size: 40.0,
+                      color: Colors.cyan[700],
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text(
+                      'Telegram : A_prog',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                        fontFamily: 'IBMPlexSansArabic',
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
